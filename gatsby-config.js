23ab4d8c6e79b3145fdf64e6cbc7aad926b9f57e
Star-Squid <81830542+Star-Squid@users.noreply.github.com>
2023-01-04 12:24:37 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Barbara, Mat, and Cat`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-google-gtag", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }]
+};

ac3dda1578784f9b0692fd9eb25f096d0635a965
Star-Squid <81830542+Star-Squid@users.noreply.github.com>
2023-01-23 16:24:30 +0000

    fontawesome brands work

--- src/barbara-components/Sidebar.js
+++ src/barbara-components/Sidebar.js
@@ -1,28 +1,33 @@
-import * as React from "react"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro' 
+import * as React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid, brands } from "@fortawesome/fontawesome-svg-core/import.macro";
 
-export default function(){
-    return(<nav class="icons">
-    <ul>
-      <li>            <FontAwesomeIcon icon={solid('user-secret')} />
-</li>
-      <li>
-        <a href="mailto:barbara@mb-malta.co.uk" aria-label="Email"
-          ><i class="fa-solid fa-envelope"></i
-        ></a>
-      </li>
-      <li>
-        <a href="https://www.linkedin.com/in/barbara-malta" aria-label="LinkedIn"
-          ><i class="fa-brands fa-linkedin"></i
-        ></a>
-      </li>
+export default function () {
+  return (
+    <nav class="icons">
+      <ul>
       
-      <li>
-        <a href="https://github.com/Star-Squid" aria-label="Github"
-          ><i class="fa-brands fa-github"></i
-        ></a>
-      </li>
-    </ul>
-  </nav>)
+        <li>
+          <a href="mailto:barbara@mb-malta.co.uk" aria-label="Email">
+            <FontAwesomeIcon icon={solid("envelope")} />
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="https://www.linkedin.com/in/barbara-malta"
+            aria-label="LinkedIn"
+          >
+            <FontAwesomeIcon icon={brands("linkedin")} />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://github.com/Star-Squid" aria-label="Github">
+            <FontAwesomeIcon icon={brands("github")} />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }

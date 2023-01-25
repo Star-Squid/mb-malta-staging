f9ed45ebc79405daa5bfd23f0e3f2bb705e48767
Star-Squid <81830542+Star-Squid@users.noreply.github.com>
2023-01-25 13:24:38 +0000

    before  mass replace

--- src/barbara-components/Navbar.js
+++ src/barbara-components/Navbar.js
@@ -1,23 +1,24 @@
-import * as React from "react"
+import * as React from "react";
 
-export default function(){
-    return(<nav class="links">
-    <ul>
-  
-      <li>
-        <a href="#header">
-          <h2 class="logo">BM</h2>
-        </a>
-      </li>
-      <li>
-        <a href="#about"> about </a>
-      </li>
-      <li>
-        <a href="#projects"> projects </a>
-      </li>
-      <li>
-        <a href="#contact"> contact </a>
-      </li>
-    </ul>
-  </nav>)
+export default function () {
+  return (
+    <nav className="links">
+      <ul>
+        <li>
+          <a href="#header">
+            <h2 class="logo">BM</h2>
+          </a>
+        </li>
+        <li>
+          <a href="#about"> about </a>
+        </li>
+        <li>
+          <a href="#projects"> projects </a>
+        </li>
+        <li>
+          <a href="#contact"> contact </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
